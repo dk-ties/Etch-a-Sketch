3b58a090d55f5368aad4f253f66bca85550f2628
dk-ties <dk.ties@gmail.com>
2023-02-03 21:59:26 +0100

    Smaller cleanup in code

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-/* function newGame {
+// on load create a sketch
+document.body.onload = newGame;
 
-} */
-
-/*Setting up function to auto insert base row on start up */
-/* import { planetVerdant } from "./planetVerdant"; */
-document.body.onload = newBase;
-
-//const planetVerdant = ["Verdant", "DW", 300, 150, 3000, "01-01", "02-02"];
-
+//get Sketch size from selector and store them in var
 gameSize = document.getElementById("language");
-console.log(typeof gameSize.value);
 gameSize = Number(gameSize.value);
-console.log(typeof gameSize);
-
-const currentDiv = document.getElementById("content");
 let elementsCount = gameSize * gameSize;
-console.log(gameSize);
-function newBase() {
+
+//get current element used in newgame function
+const currentDiv = document.getElementById("content");
+
+function newGame() {
   for (let i = 0; i < elementsCount; i++) {
     const element = document.createElement("div");
     element.classList.add("gameGrid");
     currentDiv.appendChild(element);
   }
 }
+
+//on click newGame set a new game usin the newGame function
