5c12cf21b8fb7ab7a46dc486aa321dbd62b2d559
dk-ties <dk.ties@gmail.com>
2023-02-04 13:17:48 +0100

    reset btn, add id to control gamegrid size

--- main.js
+++ main.js
@@ -2,19 +2,45 @@
 document.body.onload = newGame;
 
 //get Sketch size from selector and store them in var
-gameSize = document.getElementById("language");
-gameSize = Number(gameSize.value);
-let elementsCount = gameSize * gameSize;
+function getGameSize() {
+  gameSize = document.getElementById("language");
+  gameSize = Number(gameSize.value);
+  let elementsCount = gameSize * gameSize;
 
+  return elementsCount;
+}
 //get current element used in newgame function
 const currentDiv = document.getElementById("content");
 
 function newGame() {
+  let elementsCount = getGameSize();
+  deleteChild();
   for (let i = 0; i < elementsCount; i++) {
     const element = document.createElement("div");
     element.classList.add("gameGrid");
+    currentDiv.setAttribute("id", "gameSize" + elementsCount);
     currentDiv.appendChild(element);
   }
 }
 
 //on click newGame set a new game usin the newGame function
+document.getElementById("newGamebtn").addEventListener("click", newGame);
+//on click reset, set a new game usin the default setting function
+document.getElementById("resetGamebtn").addEventListener("click", resetGame);
+
+//on call remove all div inside parrent.
+function resetGame() {
+  deleteChild();
+  console.log("all child removed");
+  newGame();
+  console.log("new game started");
+}
+
+//Function to remove all child
+function deleteChild() {
+  let first = currentDiv.firstElementChild;
+  while (first) {
+    first.remove();
+    first = currentDiv.firstElementChild;
+  }
+}
